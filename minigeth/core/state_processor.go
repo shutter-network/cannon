97df9b912c071ba7d5cac17ad9dcaa56c3a735a0
cducrest <come.ducrest@gmail.com>
2022-04-21 13:32:04 +0200

    Execute plaintext tx after cipher tx in state processor

--- minigeth/core/state_processor.go
+++ minigeth/core/state_processor.go
@@ -176,6 +176,24 @@ func (p *StateProcessor) Process(block *types.Block, statedb *state.StateDB, cfg
 		}
 	}
 
+	// process plaintext tx
+	for i, tx := range block.Transactions() {
+		if tx.Type() == types.ShutterTxType || tx.Type() == types.BatchContextTxType {
+			continue
+		}
+		msg, err := tx.AsMessage(types.MakeSigner(p.config, header.Number), header.BaseFee)
+		if err != nil {
+			return nil, nil, 0, fmt.Errorf("could not apply tx %d [%v]: %w", i, tx.Hash().Hex(), err)
+		}
+		statedb.Prepare(tx.Hash(), i)
+		receipt, err := applyTransaction(msg, p.config, p.bc, nil, gp, statedb, blockNumber, blockHash, tx, usedGas, vmenv)
+		if err != nil {
+			return nil, nil, 0, fmt.Errorf("could not apply tx %d [%v]: %w", i, tx.Hash().Hex(), err)
+		}
+		receipts = append(receipts, receipt)
+		allLogs = append(allLogs, receipt.Logs...)
+	}
+
 	// Finalize the block, applying any consensus engine specific extras (e.g. block rewards)
 	p.engine.Finalize(p.bc, header, statedb, block.Transactions(), block.Uncles())
 
