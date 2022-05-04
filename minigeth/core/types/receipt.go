aef8fd941ba95551d83255a70928416f51bafad8
Max Langenfeld <max@brainbot.com>
2022-05-04 11:52:24 +0200

    Sync types with github.com/shutter-network/go-ethereum

--- minigeth/core/types/receipt.go
+++ minigeth/core/types/receipt.go
@@ -176,7 +176,7 @@ func (r *Receipt) DecodeRLP(s *rlp.Stream) error {
 			return errEmptyTypedReceipt
 		}
 		r.Type = b[0]
-		if r.Type == AccessListTxType || r.Type == DynamicFeeTxType || r.Type == ShutterTxType {
+		if r.Type == AccessListTxType || r.Type == DynamicFeeTxType || r.Type == ShutterTxType || r.Type == BatchTxType {
 			var dec receiptRLP
 			if err := rlp.DecodeBytes(b[1:], &dec); err != nil {
 				return err
@@ -347,6 +347,9 @@ func (rs Receipts) EncodeIndex(i int, w *bytes.Buffer) {
 		rlp.Encode(w, data)
 	case ShutterTxType:
 		w.WriteByte(ShutterTxType)
+		rlp.Encode(w, data)
+	case BatchTxType:
+		w.WriteByte(BatchTxType)
 		rlp.Encode(w, data)
 	default:
 		// For unsupported types, write nothing. Since this is for
